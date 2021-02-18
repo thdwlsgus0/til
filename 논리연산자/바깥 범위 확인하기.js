a978ee35e72d3ab6d162ffc209b4935387c07454
thdwlsgus0 <thdwlsgus0@naver.com>
2021-02-18 20:46:16 +0900

    논리연산자

---
+++ 논리연산자/바깥 범위 확인하기.js
@@ -0,0 +1,11 @@
+if(age<14 || age>90){
+  console.log(true);
+}else{
+  console.log(false);
+}
+
+if(!(age>=14 && age<=90)){
+  console.log(true);
+}else{
+  console.log(false);
+}
---
+++ 논리연산자/사이 범위 확인하기.js
@@ -0,0 +1,6 @@
+let age = 10;
+if(age>= 14 && age <= 90){
+  console.log('속합니다.')
+}else{
+  console.log('속하지 않습니다.');
+}
