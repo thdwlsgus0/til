80110f5b53022a717c3c9399cb7f8932eb00859a
thdwlsgus0 <thdwlsgus0@naver.com>
2021-03-04 15:33:44 +0900

    숫자형 학습

---
+++ 숫자형/math.js
@@ -0,0 +1,1 @@
+console.log(Math.round(6.35*10)/10);
---
+++ 숫자형/random.js
@@ -0,0 +1,5 @@
+const random = (min, max) => {
+  return min + Math.random() * (max-min);
+}
+console.log(random(1,5));
+console.log(random(1,5));
